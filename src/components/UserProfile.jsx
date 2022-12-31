fd8dfe531f0dae6cec32c586a67ff4e59e31045f
Rishav045 <82647816+Rishav045@users.noreply.github.com>
2022-12-31 17:11:32 +0530

    Update UserProfile.jsx

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -45,8 +45,8 @@ const UserProfile = () => {
     }
   }, [text, userId]);
 
-  const async logout = () => {
-   await localStorage.clear();
+  const  logout = () => {
+    localStorage.clear();
 
     navigate('/login');
   };
